8bde4f75c0328172ec3974159403e5197e8f6bee
afusox <post@liberal23.com>
2020-07-21 01:45:57 +0900

    Update main.js

--- MyOmikuji/main.js
+++ MyOmikuji/main.js
@@ -3,21 +3,17 @@
 {
   const btn = document.getElementById('btn');
   btn.addEventListener('click',()=>{
-    const results = ['大吉','中吉','小吉'];
- 
-    btn.textContent = results[Math.floor(Math.random() * results.length)];
+    // const results = ['大吉','中吉','小吉'];
+    // btn.textContent = results[Math.floor(Math.random() * results.length)];
 
-    // switch(n){
-    //   case 0:
-    //     btn.textContent = '大吉';
-    //     break
-    //   case 1:
-    //     btn.textContent = '中吉';
-    //     break
-    //   case 2:
-    //     btn.textContent = '凶';
-    //     break
-    // }
+    const n = Math.random();
+    if(n < 0.05){
+      btn.textContent = '大吉';
+    } else if(n < 0.2){
+      btn.textContent = '中吉';
+    }else{
+      btn.textContent = '凶';
+    }
 
   })
 }
